94995924991784a7fc6bfae042b5c6aa86544012
AltheaBriggs <a.briggs@sjasd.ca>
2016-10-20 10:13:00 -0500

    mark this
    Hello User2

---
+++ FrmHelloUser2.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace HelloUser2
+{
+    partial class FrmHelloUser2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDisplay2 = new System.Windows.Forms.Label();
+            this.btnDisplay1 = new System.Windows.Forms.Button();
+            this.t = new System.Windows.Forms.Button();
+            this.lblDisplay6 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtfirstNum1 = new System.Windows.Forms.TextBox();
+            this.txtsecondNum2 = new System.Windows.Forms.TextBox();
+            this.txtbutton2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lbldisplay8 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDisplay2
+            // 
+            this.lblDisplay2.Location = new System.Drawing.Point(275, 180);
+            this.lblDisplay2.Name = "lblDisplay2";
+            this.lblDisplay2.Size = new System.Drawing.Size(100, 45);
+            this.lblDisplay2.TabIndex = 0;
+            // 
+            // btnDisplay1
+            // 
+            this.btnDisplay1.Location = new System.Drawing.Point(12, 16);
+            this.btnDisplay1.Name = "btnDisplay1";
+            this.btnDisplay1.Size = new System.Drawing.Size(215, 23);
+            this.btnDisplay1.TabIndex = 1;
+            this.btnDisplay1.Text = "Click this button if you feel unmotivated";
+            this.btnDisplay1.UseVisualStyleBackColor = true;
+            this.btnDisplay1.Click += new System.EventHandler(this.btnDisplay1_Click);
+            // 
+            // t
+            // 
+            this.t.Location = new System.Drawing.Point(12, 74);
+            this.t.Name = "t";
+            this.t.Size = new System.Drawing.Size(133, 23);
+            this.t.TabIndex = 3;
+            this.t.Text = "String method";
+            this.t.UseVisualStyleBackColor = true;
+            this.t.Click += new System.EventHandler(this.t_Click);
+            // 
+            // lblDisplay6
+            // 
+            this.lblDisplay6.AutoSize = true;
+            this.lblDisplay6.Location = new System.Drawing.Point(192, 79);
+            this.lblDisplay6.Name = "lblDisplay6";
+            this.lblDisplay6.Size = new System.Drawing.Size(0, 13);
+            this.lblDisplay6.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Arithemetic";
+            // 
+            // txtfirstNum1
+            // 
+            this.txtfirstNum1.Location = new System.Drawing.Point(23, 163);
+            this.txtfirstNum1.Name = "txtfirstNum1";
+            this.txtfirstNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtfirstNum1.TabIndex = 6;
+            // 
+            // txtsecondNum2
+            // 
+            this.txtsecondNum2.Location = new System.Drawing.Point(23, 205);
+            this.txtsecondNum2.Name = "txtsecondNum2";
+            this.txtsecondNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtsecondNum2.TabIndex = 7;
+            // 
+            // txtbutton2
+            // 
+            this.txtbutton2.Location = new System.Drawing.Point(170, 180);
+            this.txtbutton2.Name = "txtbutton2";
+            this.txtbutton2.Size = new System.Drawing.Size(75, 23);
+            this.txtbutton2.TabIndex = 8;
+            this.txtbutton2.Text = "calculate";
+            this.txtbutton2.UseVisualStyleBackColor = true;
+            this.txtbutton2.Click += new System.EventHandler(this.txtbutton2_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(42, 271);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Input and Output";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(69, 305);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(84, 336);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // lbldisplay8
+            // 
+            this.lbldisplay8.AutoSize = true;
+            this.lbldisplay8.Location = new System.Drawing.Point(18, 308);
+            this.lbldisplay8.Name = "lbldisplay8";
+            this.lbldisplay8.Size = new System.Drawing.Size(38, 13);
+            this.lbldisplay8.TabIndex = 12;
+            this.lbldisplay8.Text = "Name:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(-2, 339);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Favourite Colour:";
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(228, 316);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(51, 32);
+            this.btnClick.TabIndex = 14;
+            this.btnClick.Text = "click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(245, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 15;
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(310, 316);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(126, 73);
+            this.lblResult.TabIndex = 16;
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // FrmHelloUser2
+            // 
+            this.ClientSize = new System.Drawing.Size(467, 433);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbldisplay8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtbutton2);
+            this.Controls.Add(this.txtsecondNum2);
+            this.Controls.Add(this.txtfirstNum1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblDisplay6);
+            this.Controls.Add(this.t);
+            this.Controls.Add(this.btnDisplay1);
+            this.Controls.Add(this.lblDisplay2);
+            this.Name = "FrmHelloUser2";
+            this.Text = "Hello User";
+            this.Load += new System.EventHandler(this.FrmHelloUser2_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btndisplay;
+        private System.Windows.Forms.Label lblbutton;
+        private System.Windows.Forms.Button txtbutton;
+        private System.Windows.Forms.Label lbldisplay1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button txtsum;
+        private System.Windows.Forms.TextBox txtfirstNum;
+        private System.Windows.Forms.Label lblfirstNum;
+        private System.Windows.Forms.Label lblsecondNum;
+        private System.Windows.Forms.TextBox txtsecondNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbldisplay3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbldisplay;
+        private System.Windows.Forms.Label txtdisplay;
+        private System.Windows.Forms.Label lbldisplay4;
+        private System.Windows.Forms.Label lbldisplayName;
+        private System.Windows.Forms.Label lbldisplay5;
+        private System.Windows.Forms.TextBox txtbox1;
+        private System.Windows.Forms.TextBox txtbox2;
+        private System.Windows.Forms.Label lblDisplay2;
+        private System.Windows.Forms.Button btnDisplay1;
+        private System.Windows.Forms.Button t;
+        private System.Windows.Forms.Label lblDisplay6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtfirstNum1;
+        private System.Windows.Forms.TextBox txtsecondNum2;
+        private System.Windows.Forms.Button txtbutton2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lbldisplay8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ FrmHelloUser2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloUser2
+{
+    public partial class FrmHelloUser2 : Form
+    {
+        public FrmHelloUser2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtbutton2_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void FrmHelloUser2_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void FrmHelloUser2_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void txtbutton2_Click_1(object sender, EventArgs e)
+        {
+            double firstNum = Convert.ToDouble(txtfirstNum1.Text);
+            double secondNum = Convert.ToDouble(txtsecondNum2.Text);
+            double sum = firstNum + secondNum;
+             
+            lblDisplay2.Text = "Your sum is:" + sum;
+
+
+
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "favourite colour is";
+            txtInput.Text = "";
+        }
+
+        private void t_Click(object sender, EventArgs e)
+        {
+            
+                lblDisplay6.Text = "This is a string method";
+        }
+
+        private void btnDisplay1_Click(object sender, EventArgs e)
+        {
+            btnDisplay1.Text = "Keep on being motivated";
+        }
+    }
+}
+/**
+
+ *
+
+ * Name:Althea Briggs 
+
+ * Teacher: Mr.Hardman
+
+ * Assignment #, Program #
+
+ * Date Last Modified: Oct.18
+
+ *
+
+ */
